fe8e878e5507be2e0dd8f72ed9ddc8dc6dcaec1b
Chris Lamb <chris@chris-lamb.co.uk>
2008-03-09 22:32:06 +0000

    local.js: Add fourth band.
    Signed-off-by: Chris Lamb <chris@chris-lamb.co.uk>

--- support/local.js
+++ support/local.js
@@ -6,6 +6,7 @@ function onLoad() {
 	theme.event.tape.height = 3.0;
 	theme.event.instant.icon = 'support/debian_sm.png';
 	theme.event.duration.color = '#FF6666';
+	theme.ether.backgroundColors[3] = "#E8E8F4";
 
 	var events = new Timeline.DefaultEventSource();
 	var releases = new Timeline.DefaultEventSource();
@@ -31,17 +32,28 @@ function onLoad() {
 		}),
 		Timeline.createBandInfo({
 			eventSource:    updates,
-			width:          "40%", 
+			width:          "23%", 
 			intervalUnit:   Timeline.DateTime.YEAR, 
 			intervalPixels: 500,
 			date:		date,
 			theme:		theme
+		}),
+		Timeline.createBandInfo({
+			eventSource:    updates,
+			width:          "7%", 
+			intervalUnit:   Timeline.DateTime.YEAR, 
+			intervalPixels: 70,
+			date:		date,
+			theme:		theme,
+			overview:	true,
 		})
 	];
 	bandInfos[1].highlight = true;
 	bandInfos[1].syncWith = 0;
 	bandInfos[2].highlight = true;
 	bandInfos[2].syncWith = 0;
+	bandInfos[3].highlight = true;
+	bandInfos[3].syncWith = 0;
 
 	tl = Timeline.create(document.getElementById("the-timeline"), bandInfos);
 	Timeline.loadXML("data/events.xml", function(xml, url) { events.loadXML(xml, url); });
